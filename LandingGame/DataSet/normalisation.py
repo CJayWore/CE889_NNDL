691a446265aaffd56166a000f24700b1e8a56ede
Jiwei Chen <jc23746@essex.ac.uk>
2024-12-03 17:35:43 +0000

    KFold complete

--- LandingGame/DataSet/normalisation.py
+++ LandingGame/DataSet/normalisation.py
@@ -1,20 +1,27 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+
+source_df = pd.read_csv('raw_data.csv')
+x = len(source_df)//5
+testing_df = source_df.sample(n=x) # 随机选取x行
+
+# 删除源数据中已选取的行
+training_df = source_df.drop(testing_df.index)
+training_df.to_csv('train.csv', index=False)
+testing_df.to_csv('test.csv', index=False)
 
 
-def scale_csv_data(input_csv, output_csv, min_max):
-    df = pd.read_csv(input_csv)
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    df_scaled = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
-    df_scaled.to_csv(output_csv, index=False)
-    min_max_values = pd.DataFrame({
-        'column':df.columns,
-        'min':scaler.data_min_,
-        'max':scaler.data_max_,
-    })
-    min_max_values.to_csv(min_max, index=False)
+# Normalization:
+def min_max_normalize(df, columns_to_normalize):
+    result = df.copy()
+    for column in columns_to_normalize:
+        max_value = df[column].max()
+        min_value = df[column].min()
+        result[column] = (df[column] - min_value) / (max_value - min_value)
+    return result
 
-input_csv = 'raw_data.csv'
-output_csv = 'scaled_data.csv'
-min_max = 'min_max_values.csv'
-scale_csv_data(input_csv, output_csv, min_max)
+normalization_method = 'min_max_normalize'
+columns_to_normalize = training_df.columns.tolist()
+
+normalized_data = min_max_normalize(training_df, columns_to_normalize)
+normalized_data.to_csv('normalized_data.csv', index=False)
+
