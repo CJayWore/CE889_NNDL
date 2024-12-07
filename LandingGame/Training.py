7779a0daf5fa90f195ff118295e5a72bca97181b
Jiwei <CJayWore@outlook.com>
2024-12-07 15:05:57 +0000

    Update Training.py

--- LandingGame/Training.py
+++ LandingGame/Training.py
@@ -23,14 +23,17 @@ def save_predictions(inputs, predicted_output, targets, filename):
 train_normalized = pd.read_csv('DataSet/train_normalized.csv', header=0).to_numpy()
 test_normalized = pd.read_csv('DataSet/test_normalized.csv', header=0).to_numpy()
 
-nn = NN.NeuralNetwork(learning_rate=0.00505, momentum=0.55)
+nn = NN.NeuralNetwork(learning_rate=0.01, momentum=0.1)
 
 logging.info(f'TrainingData_shape: {train_normalized.shape}, TestingData_shape: {test_normalized.shape}')
 logging.info(f'Network: learning rate={nn.learning_rate}, momentum={nn.momentum}')
+print(f'TrainingData_shape: {train_normalized.shape}, TestingData_shape: {test_normalized.shape}')
+print(f'Network: learning rate={nn.learning_rate}, momentum={nn.momentum}')
 
 # training
 epochs = 100
 logging.info('Training in process')
+print('Training in process')
 
 
 inputs_train = train_normalized[:,:2]
@@ -50,15 +53,16 @@ for epoch in range(epochs):
         total_gradient_output += np.array(gradient_output)
 
     logging.info(f'     Epoch {epoch+1}, Loss: {total_loss}, Gradient_Hidden: {total_gradient_hidden}, Gradient_Output: {total_gradient_output}')
-
+    print(f'Epoch {epoch + 1}, Loss: {total_loss}, Gradient_Hidden: {total_gradient_hidden}, Gradient_Output: {total_gradient_output}')
     if total_loss < best_loss:
         best_loss = total_loss
-        nn.save_weights('best_weights_txt')
+        nn.save_weights('best_weights.txt')
 # Testing
 total_predictions = len(inputs_test)
 predicted_outputs = []  # 存储所有测试样本的预测结果
 
 logging.info('Testing in process')
+print('Testing in process')
 
 # RMSE
 sum_se = 0
@@ -71,6 +75,7 @@ for inputs, real_output in zip(inputs_test, outputs_test):
 predicted_outputs = np.array(predicted_outputs)
 rmse = np.sqrt(np.mean((predicted_outputs - outputs_test) ** 2))
 logging.info(f'RMSE: {rmse}')
+print(f'RMSE: {rmse}')
 
 save_predictions(
     inputs_test , predicted_outputs, outputs_test, filename=f"Predictions/prediction_RMSE_{rmse:.4f}.csv"
